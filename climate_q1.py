39de63ecf2bace716c39a0d1bd6194782ce82db6
Ben White <ben.white@griffithuni.edu.au>
2023-09-13 22:38:48 +1000

    climate_q1 and climate_q2 finished

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,26 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
+
+con = sqlite3.connect('climate.db')
+
+cursor = con.cursor()
+
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+
 years = []
 co2 = []
 temp = []
+
+for row in cursor.fetchall():
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+print("Years:", years)
+print("CO2:", co2)
+print("Temperature:", temp)
+
+con.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+climate_df = pd.read_csv('climate.csv')
+
+years = climate_df['Year'].tolist()
+co2 = climate_df['CO2'].tolist()
+temp = climate_df['Temperature'].tolist()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
